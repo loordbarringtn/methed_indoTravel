e8684a6b77dca27c7225f305d48938e47300b321
loordbarringtn <quantas@gmail.com>
2023-04-21 01:52:14 +0300

    refactoring again

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ const setDeadline = (setDeadline) => {
   timer.dataset.timerDeadline = setDeadline;
 };
 
-const defineWords = (number, words) => {
-    const lastDigit = number % 10;    
-    if (number >= 11 && number <= 14) {
-      return words[2];
-    } else if (lastDigit === 1) {
-      return words[0];
-    } else if (lastDigit >= 2 && lastDigit <= 4) {
-      return words[1];
-    } else {
-      return words[2];
-    }
-  };
+const getWordByNumber = (number, words) => {
+  const lastDigit = number % 10;
+  if (number >= 11 && number <= 14) {
+    return words[2];
+  } else if (lastDigit === 1) {
+    return words[0];
+  } else if (lastDigit >= 2 && lastDigit <= 4) {
+    return words[1];
+  } else {
+    return words[2];
+  }
+};
 
 const timer = () => {
   const timer = document.querySelector(".timer");
@@ -32,10 +32,7 @@ const timer = () => {
   const getTimeRemaining = () => {
     const dateStop = new Date(timer.dataset.timerDeadline).getTime();
     const dateNow = Date.now();
-
-    const timezoneOffset = 3 * 60 * 60 * 1000;
-    const timeRemaining = (dateStop - dateNow + timezoneOffset) / 1000;
-
+    let timeRemaining = (dateStop - dateNow) / 1000;
     const minutes = Math.floor((timeRemaining / 60) % 60);
     const hours = Math.floor((timeRemaining / 60 / 60) % 24);
     const days = Math.floor(timeRemaining / 60 / 60 / 24);
@@ -57,11 +54,23 @@ const timer = () => {
     let timer = getTimeRemaining();
 
     daysValue.textContent = timer.days;
-    daysLabel.textContent = defineWords(timer.days, ["день", "дня", "дней"]);
+    daysLabel.textContent = getWordByNumber(timer.days, [
+      "день",
+      "дня",
+      "дней",
+    ]);
     hoursValue.textContent = timer.hours;
-    hoursLabel.textContent = defineWords(timer.hours, ["час", "часа", "часов"]);
+    hoursLabel.textContent = getWordByNumber(timer.hours, [
+      "час",
+      "часа",
+      "часов",
+    ]);
     minutesValue.textContent = timer.minutes;
-    minutesLabel.textContent = defineWords(timer.minutes, ["минута", "минуты", "минут"]);
+    minutesLabel.textContent = getWordByNumber(timer.minutes, [
+      "минута",
+      "минуты",
+      "минут",
+    ]);
 
     let interValId = setInterval(updateClock, 60000);
 
@@ -73,12 +82,26 @@ const timer = () => {
       advertText.style.display = "none";
       timerText.style.display = "none";
     } else if (timer.timeRemaining / 3600 <= 24) {
+      const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+      timer.timeRemaining += timezoneOffset;
       daysValue.textContent = timer.hours;
-      daysLabel.textContent = defineWords(timer.hours, ["час", "часа", "часов"]);
+      daysLabel.textContent = getWordByNumber(timer.hours, [
+        "час",
+        "часа",
+        "часов",
+      ]);
       hoursValue.textContent = timer.minutes;
-      hoursLabel.textContent = defineWords(timer.minutes, ["минута", "минуты", "минут"]);
+      hoursLabel.textContent = getWordByNumber(timer.minutes, [
+        "минута",
+        "минуты",
+        "минут",
+      ]);
       minutesValue.textContent = timer.seconds;
-      minutesLabel.textContent = defineWords(timer.seconds, ["секунда", "секунды", "секунд"]);
+      minutesLabel.textContent = getWordByNumber(timer.seconds, [
+        "секунда",
+        "секунды",
+        "секунд",
+      ]);
       interValId = setInterval(updateClock, 1000);
     }
   };
@@ -96,9 +119,7 @@ setDeadline('2023-04-30');
 // setDeadline(new Date().toISOString().slice(0, 10));
 // setDeadline("2023-04-20 14:45:00");
 // setDeadline("2023-04-21");
+// setDeadline("2023-04-21 11:45:00");
+
 // setDeadline("2023-04-20 11:45:00");
 timer();
-
-
-
-
