6bcdbd035a571cabb1e999234ec1f25d1b1b4b10
loordbarringtn <quantas@gmail.com>
2023-04-21 11:38:56 +0300

    timeout refactoring

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+const timerSection = document.querySelector(".timer");
+const daysLabel = document.querySelector(".timer__units_days");
+const hoursLabel = document.querySelector(".timer__units_hours");
+const minutesLabel = document.querySelector(".timer__units_minutes");
+const advertText = document.querySelector(".hero__text");
+const timerText = document.querySelector(".hero__timer");
+const daysValue = document.querySelector(".timer__count_days");
+const hoursValue = document.querySelector(".timer__count_hours");
+const minutesValue = document.querySelector(".timer__count_minutes");
+
 const setDeadline = (setDeadline) => {
-  const timer = document.querySelector(".timer");
-  timer.dataset.timerDeadline = setDeadline;
+  timerSection.dataset.timerDeadline = setDeadline;
 };
 
 const getWordByNumber = (number, words) => {
@@ -17,99 +26,89 @@ const getWordByNumber = (number, words) => {
 };
 
 const timer = () => {
-  const timer = document.querySelector(".timer");
-  const daysLabel = document.querySelector(".timer__units_days");
-  const hoursLabel = document.querySelector(".timer__units_hours");
-  const minutesLabel = document.querySelector(".timer__units_minutes");
-
-  const advertText = document.querySelector(".hero__text");
-  const timerText = document.querySelector(".hero__timer");
-
-  const daysValue = document.querySelector(".timer__count_days");
-  const hoursValue = document.querySelector(".timer__count_hours");
-  const minutesValue = document.querySelector(".timer__count_minutes");
-
-  const getTimeRemaining = () => {
-    const dateStop = new Date(timer.dataset.timerDeadline).getTime();
-    const dateNow = Date.now();
-    let timeRemaining = (dateStop - dateNow) / 1000;
-    const minutes = Math.floor((timeRemaining / 60) % 60);
-    const hours = Math.floor((timeRemaining / 60 / 60) % 24);
-    const days = Math.floor(timeRemaining / 60 / 60 / 24);
-    const seconds = Math.floor(timeRemaining % 60);
-
-    return {
-      timeRemaining,
-      days,
-      hours,
-      minutes,
-      daysLabel,
-      hoursLabel,
-      minutesLabel,
-      seconds,
+    let intervalId;
+  
+    const getTimeRemaining = () => {
+      const dateStop = new Date(timerSection.dataset.timerDeadline).getTime();
+      const dateNow = Date.now();
+      let timeRemaining = (dateStop - dateNow) / 1000;
+      const minutes = Math.floor((timeRemaining / 60) % 60);
+      const hours = Math.floor((timeRemaining / 60 / 60) % 24);
+      const days = Math.floor(timeRemaining / 60 / 60 / 24);
+      const seconds = Math.floor(timeRemaining % 60);
+  
+      return {
+        timeRemaining,
+        days,
+        hours,
+        minutes,
+        daysLabel,
+        hoursLabel,
+        minutesLabel,
+        seconds,
+      };
     };
-  };
-
-  const updateClock = () => {
-    let timer = getTimeRemaining();
-
-    daysValue.textContent = timer.days;
-    daysLabel.textContent = getWordByNumber(timer.days, [
-      "день",
-      "дня",
-      "дней",
-    ]);
-    hoursValue.textContent = timer.hours;
-    hoursLabel.textContent = getWordByNumber(timer.hours, [
-      "час",
-      "часа",
-      "часов",
-    ]);
-    minutesValue.textContent = timer.minutes;
-    minutesLabel.textContent = getWordByNumber(timer.minutes, [
-      "минута",
-      "минуты",
-      "минут",
-    ]);
-
-    let interValId = setInterval(updateClock, 60000);
-
-    if (timer.timeRemaining <= 0) {
-      clearInterval(interValId);
-      daysValue.textContent = "00";
-      hoursValue.textContent = "00";
-      minutesValue.textContent = "00";
-      advertText.style.display = "none";
-      timerText.style.display = "none";
-    } else if (timer.timeRemaining / 3600 <= 24) {
-      const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
-      timer.timeRemaining += timezoneOffset;
-      daysValue.textContent = timer.hours;
-      daysLabel.textContent = getWordByNumber(timer.hours, [
+  
+    const updateClock = () => {
+      let timer = getTimeRemaining();
+  
+      daysValue.textContent = timer.days;
+      daysLabel.textContent = getWordByNumber(timer.days, [
+        "день",
+        "дня",
+        "дней",
+      ]);
+      hoursValue.textContent = timer.hours;
+      hoursLabel.textContent = getWordByNumber(timer.hours, [
         "час",
         "часа",
         "часов",
       ]);
-      hoursValue.textContent = timer.minutes;
-      hoursLabel.textContent = getWordByNumber(timer.minutes, [
+      minutesValue.textContent = timer.minutes;
+      minutesLabel.textContent = getWordByNumber(timer.minutes, [
         "минута",
         "минуты",
         "минут",
       ]);
-      minutesValue.textContent = timer.seconds;
-      minutesLabel.textContent = getWordByNumber(timer.seconds, [
-        "секунда",
-        "секунды",
-        "секунд",
-      ]);
-      interValId = setInterval(updateClock, 1000);
-    }
+  
+      if (timer.timeRemaining <= 0) {
+        clearInterval(intervalId);
+        daysValue.textContent = "00";
+        hoursValue.textContent = "00";
+        minutesValue.textContent = "00";
+        advertText.style.display = "none";
+        timerText.style.display = "none";
+      } else if (timer.timeRemaining / 3600 <= 24) {
+        clearInterval(intervalId);
+        intervalId = setInterval(updateClock, 1000);
+        const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+        timer.timeRemaining += timezoneOffset;
+        daysValue.textContent = timer.hours;
+        daysLabel.textContent = getWordByNumber(timer.hours, [
+          "час",
+          "часа",
+          "часов",
+        ]);
+        hoursValue.textContent = timer.minutes;
+        hoursLabel.textContent = getWordByNumber(timer.minutes, [
+          "минута",
+          "минуты",
+          "минут",
+        ]);
+        minutesValue.textContent = timer.seconds;
+        minutesLabel.textContent = getWordByNumber(timer.seconds, [
+          "секунда",
+          "секунды",
+          "секунд",
+        ]);
+      }
+    };
+    updateClock();
+    intervalId = setInterval(updateClock, 60000);
   };
-  updateClock();
-};
-
+  
 //Для тестирования
-setDeadline('2023-04-30');
+// setDeadline('2023-04-30');
 // setDeadline('2023-04-28 23:59:59');
 // setDeadline('2023-04-28 21:59:59');
 // setDeadline('2023-04-28 21:37:59');
@@ -118,8 +117,9 @@ setDeadline('2023-04-30');
 // setDeadline(new Date().toLocaleDateString());
 // setDeadline(new Date().toISOString().slice(0, 10));
 // setDeadline("2023-04-20 14:45:00");
-// setDeadline("2023-04-21");
+setDeadline("2023-04-22");
 // setDeadline("2023-04-21 11:45:00");
 
 // setDeadline("2023-04-20 11:45:00");
 timer();
+
